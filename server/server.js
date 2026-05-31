817496c677d285081a7f78748fe45f73c7cb2e0e
amoremea <danil2005danil@bk.ru>
2026-05-31 12:22:16 +0500

    cors...

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://gmessanger.onrender.com',
+  'https://gigamessanger.onrender.com',
   'file://'
 ];
 
